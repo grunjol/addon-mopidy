b6b19318dd3b916acf996ded59de603714b54925
grunjol <grunjol@gmail.com>
2021-04-06 12:28:25 -0300

    move media directory from shared to HA media dir

--- mopidy/rootfs/etc/cont-init.d/20-folders.sh
+++ mopidy/rootfs/etc/cont-init.d/20-folders.sh
@@ -3,7 +3,7 @@
 # Home Assistant Community Add-on: Mopidy
 # Ensures needed folder exists.
 # ==============================================================================
-readonly MEDIA_DIR="/share/mopidy/media"
+readonly MEDIA_DIR="/media"
 
 if ! bashio::fs.directory_exists "${MEDIA_DIR}"; then
   mkdir -p "${MEDIA_DIR}" || bashio::exit.nok "Could not create media folder: ${MEDIA_DIR}"
